e876c959813dd0b949fd558759c4b2ac5b21ecb7
Fauzan Bintang <obin.aji@gmail.com>
2022-06-09 22:59:19 +0700

    learn switch, const, and if with using testing

--- fundamentals/hello.go
+++ fundamentals/hello.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
 }
 
-func main() {
-	fmt.Println(Hello("Chris"))
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
+
+// func main() {
+// fmt.Println(Hello("Chris"))
+// }
