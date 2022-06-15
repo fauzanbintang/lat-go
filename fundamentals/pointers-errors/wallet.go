43535d22e4bd57ec13f77888f9b8a0508b8c45cc
Fauzan Bintang <obin.aji@gmail.com>
2022-06-15 13:44:38 +0700

    feat: pointers, errors, structs, method, interfaces

--- fundamentals/pointers-errors/wallet.go
+++ fundamentals/pointers-errors/wallet.go
@@ -20,9 +20,7 @@ var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
-
 func (w *Wallet) Withdraw(amount Bitcoin) error {
-
 	if amount > w.balance {
 		return ErrInsufficientFunds
 	}
@@ -30,11 +28,9 @@ func (w *Wallet) Withdraw(amount Bitcoin) error {
 	w.balance -= amount
 	return nil
 }
-
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
-
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
--- fundamentals/structs-methods-interfaces/shapes.go
+++ fundamentals/structs-methods-interfaces/shapes.go
@@ -31,6 +31,7 @@ func (t Triangle) Area() float64 {
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
-}
+
+// func Area(rectangle Rectangle) float64 {
+// 	return rectangle.Width * rectangle.Height
+// }
